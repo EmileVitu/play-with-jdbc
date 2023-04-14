fd183d1bfdcec77ee9228793a6d9b8fcbcaa73bd
EmileVitu <75259154+EmileVitu@users.noreply.github.com>
2023-04-14 10:23:39 +0200

    Created the buffered reader

--- play-with-jdbc/src/main/java/org/vitu/jdbc/PlayWithMaire.java
+++ play-with-jdbc/src/main/java/org/vitu/jdbc/PlayWithMaire.java
@@ -1,10 +1,56 @@
 package org.vitu.jdbc;
 
-public class PlayWithMaire {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.vitu.jdbc.model.Commune;
+
+public class PlayWithCommune {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Map<String, Commune> communes = new HashMap<>();
+		
+		Path path = Path.of("data/maires-25-04-2014.csv");
+		try(BufferedReader reader = Files.newBufferedReader(path);) {
+			
+			String line = reader.readLine();
+			line = reader.readLine();
+			
+			while (line != null) {
+				
+				String[] split = line.split(";");
+				
+				String codeDepartement = split[0];
+				if (codeDepartement.length() == 1) {
+					codeDepartement = "0" + codeDepartement;
+				}
+				
+				String codeInsee = split[2];
+				if (codeInsee.length() == 1) {
+					codeInsee = "00" + codeInsee;
+				} else if (codeInsee.length() == 2) {
+					codeInsee = "0" + codeInsee;
+				}
+				
+				String codePostal = codeDepartement + codeInsee;
+				String nom = split[3];
+				
+				Commune commune = new Commune(codePostal, nom);
+				
+				communes.put(codePostal, commune);
+				
+				line = reader.readLine();
+			
+			}
+		
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		
 	}
-
 }
---
+++ play-with-jdbc/src/main/java/org/vitu/jdbc/model/Commune.java
@@ -0,0 +1,34 @@
+package org.vitu.jdbc.model;
+
+public class Commune {
+
+	private String codePostal;
+	private String nom;
+
+	public Commune(String codePostal, String nom) {
+		this.codePostal = codePostal;
+		this.nom = nom;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	@Override
+	public String toString() {
+		return "Commune [codePostal=" + codePostal + ", nom=" + nom + "]";
+	}
+
+}
